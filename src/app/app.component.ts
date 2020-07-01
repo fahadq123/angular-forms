dc4fb3282fb0de43470c0d5d550af64e88547ba0
Fahad Qayyum <fahadq@my.yorku.ca>
2020-07-01 00:14:09 -0400

    Form validation and data retrieval added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
-import {NgForm} from '@angular/forms';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm, NgModelGroup} from '@angular/forms';
+import {stringify} from 'querystring';
 
 @Component({
     selector: 'app-root',
@@ -7,13 +8,57 @@ import {NgForm} from '@angular/forms';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    @ViewChild('formData', {static: true}) formData: NgForm;
+    @ViewChild('formGroup', {static: true}) groupData: NgModelGroup;
+
+    userInfo = {
+        username: '',
+        email: '',
+        secret: '',
+        gender: ''
+    };
+    isSubmitted = false;
+
+    genders: string[] = [
+        'Male',
+        'Female'
+    ];
 
     suggestUserName() {
-        const suggestedName = 'Superuser';
+        // user setValue only when u wanna update all the field,
+        // the structure must be the same as that of the table
+
+        // this.formData.setValue({
+        //     formGroup: {
+        //         username: 'Fahad',
+        //         email: 'fahadq@developer.com',
+        //         secret: 'teacher',
+        //         gender: 'Male'
+        //     }
+        // });
+
+        // user patchValue if you dont wanna update all the fields
+        this.formData.form.patchValue({
+            formGroup: {
+                username: 'Fahad'
+            }
+        });
+
     }
 
     onFormSubmit(formData: NgForm) {
+        this.isSubmitted = true;
         console.log(formData);
-    }
 
+        // to print the value in the formData
+        console.log(this.groupData.value.email);
+        console.log('is form data valid: ' + formData.valid);
+
+
+        this.userInfo.username = this.formData.value.formGroup.username;
+        this.userInfo.email = this.formData.value.formGroup.email;
+        this.userInfo.secret = this.formData.value.secret;
+        this.userInfo.gender = this.formData.value.gender;
+        this.formData.reset();
+    }
 }
